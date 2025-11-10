15209c6a750895aef9f38badf439e00b16e5fa02
OFFICEVNPAY\linhtvc <linhtvc@vnpay.vn>
2025-11-10 15:54:35 +0700

    add BubbleSortExample.cs

---
+++ C#/ConsoleApp/DSA/Arrays/BubbleSortExample.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp.DSA;
+
+public class BubbleSortExample
+{
+    public static void Main(string[] arg)
+    {
+        int[] intArr = { 7, 12, 9, 11, 3 };
+        for (int i = 0; i <= intArr.Length - 2; i++)
+        {
+            for (int j = i+1; j <= intArr.Length - 1; j++)
+            {
+                int intTemp = intArr[i];
+                if (intArr[j] < intTemp)
+                {
+                    intArr[i] = intArr[j];
+                    intArr[j] = intTemp;
+                }
+            }
+        }
+
+        foreach(var item in intArr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
--- C#/ConsoleApp/DSA/Arrays/FindLowestExample.cs
+++ C#/ConsoleApp/DSA/Arrays/FindLowestExample.cs
@@ -2,17 +2,17 @@
 
 public static class FindLowestExample
 {
-    public static void Main(string[] args)
-    {
-        int[] arrInt = { 7, 12, 9, 4, 11 };
-        int lowest = arrInt[0];
-        for (int i = 0; i < arrInt.Length-1; i++)
-        {
-            if(lowest > arrInt[i])
-            {
-                lowest = arrInt[i];
-            }
-        }
-        Console.WriteLine(lowest);
-    } 
+    //public static void Main(string[] args)
+    //{
+    //    int[] arrInt = { 7, 12, 9, 4, 11 };
+    //    int lowest = arrInt[0];
+    //    for (int i = 0; i < arrInt.Length-1; i++)
+    //    {
+    //        if(lowest > arrInt[i])
+    //        {
+    //            lowest = arrInt[i];
+    //        }
+    //    }
+    //    Console.WriteLine(lowest);
+    //} 
 }
